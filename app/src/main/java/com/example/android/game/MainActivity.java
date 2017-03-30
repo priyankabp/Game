aea54091717f13e703145d30c2e9b8f5b54883ad
priyankabp <pbphapale@gmail.com>
2017-03-30 01:32:34 -0400

    Add Login page.

--- app/src/main/java/com/example/android/game/MainActivity.java
+++ app/src/main/java/com/example/android/game/MainActivity.java
@@ -8,10 +8,13 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button playButton;
+    private Button registerButton;
     MediaPlayer waves;
 
     @Override
@@ -19,13 +22,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById((R.id.playButton)).setOnClickListener(new MyLysnr());
+        findViewById((R.id.registerButton)).setOnClickListener(new MyLysnr());
+
         //starts the background music for the game
         waves = MediaPlayer.create(MainActivity.this, R.raw.gametrack);
         waves.start();
     }
 
+
     public void onPlayClick(View view) {
         Intent intent = new Intent(this, StagesActivity.class);
         startActivity(intent);
+    }
+
+    private class MyLysnr implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if(v.getId()==R.id.playButton){
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+            if(v.getId()==R.id.registerButton){
+
+            }
+        }
     }
 }
